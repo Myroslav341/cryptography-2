c331844fd112d0d48225e4ec8bb512928f786586
Myroslav341 <gurmann341@gmail.com>
2020-03-14 12:27:23 +0200

    lab2

---
+++ 2/helpers.py
@@ -0,0 +1,50 @@
+from math import gcd
+import random
+
+
+def random_prime(bit_size):
+    a = random.randint(2 ** (bit_size - 1), 2 ** bit_size - 1)
+    if a % 2 == 0:
+        a += 1
+    while not miller_rabin(a):
+        a = random.randint(2 ** (bit_size - 1), 2 ** bit_size - 1)
+        if a % 2 == 0:
+            a += 1
+    return a
+
+
+def miller_rabin(n, k=10):
+    s = 0
+    t = n - 1
+    while t % 2 == 0:
+        t = t // 2
+        s += 1
+
+    for i in range(k):
+        a = random.randint(2, n - 1)
+        x = pow(a, t, n)
+        if x == 1 or x == n - 1:
+            continue
+        ok = False
+        for j in range(s - 1):
+            x = x ** 2 % n
+            if x == 1:
+                return False
+            if x == n - 1:
+                ok = True
+                break
+        if ok:
+            continue
+        else:
+            return False
+    return True
+
+
+def bezout(a, b):
+    x, xx, y, yy = 1, 0, 0, 1
+    while b:
+        q = a // b
+        a, b = b, a % b
+        x, xx = xx, x - xx*q
+        y, yy = yy, y - yy*q
+    return x, y, a
---
+++ 2/main.py
@@ -0,0 +1,39 @@
+import random
+from helpers import random_prime, bezout
+from sympy import isprime
+import json
+
+bit_size = 64
+
+while True:
+    p, q = random_prime(bit_size), random_prime(bit_size)
+
+    print(f'p = {p}:{isprime(p)}, q = {q}:{isprime(p)}')
+
+    n = q * p
+
+    print(f'n = {n}')
+
+    phi = (q - 1) * (p - 1)
+
+    e = random.randint(2, phi - 1)
+
+    print(e)
+
+    x, y, g = bezout(e, phi)
+
+    print(f'g = {g}')
+
+    d = (x % phi + phi) % phi
+
+    if g == 1:
+        break
+
+print(f'public key = ({e},{n})')
+print(f'private key = ({d},{n})')
+f = open('keys.json', 'w')
+data = dict(
+    public=(e, n),
+    private=(d, n)
+)
+json.dump(data, f)
